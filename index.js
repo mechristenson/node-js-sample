99b065bebb9b53ededb7a9ae5b946ca32fed211a
Mark Christenson <markchristenson@livenation.com>
2016-01-30 17:19:10 -0800

    Add login form to index page

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var fs = require('fs');
+var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+  displayForm(response)
+});
+
+function displayForm(res) {
+    fs.readFile('form.html', function (err, data) {
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+            'Content-Length': data.length
+        });
+        res.write(data);
+        res.end();
+    });
+}
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+});
